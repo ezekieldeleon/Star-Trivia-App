ba718b907eb5dda24e8eae237dbd7f4819ce48ff
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-09-09 09:12:51 -0700

    Set up.

---
+++ StarTrivia/StarTrivia/SelectPersonVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Ezekiel De Leon on 9/9/18.
+//  Copyright © 2018 Ezekiel De Leon. All rights reserved.
+//
+
+import UIKit
+
+class SelectPersonVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
