c53180a789e81185ae7680abd875af6316e67253
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-10-08 05:59:18 -0700

    Finished Parsing JSON.

---
+++ StarTrivia/StarTrivia/Model/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  StarTrivia
+//
+//  Created by Ezekiel De Leon on 10/6/18.
+//  Copyright © 2018 Ezekiel De Leon. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let height: String
+    let mass: String
+    let hair: String
+    let birthYear: String
+    let gender: String
+    let homeworldUrl: String
+    let filmUrls: [String]
+    let vehicleUrls: [String]
+    let starshipUrls: [String]
+}
