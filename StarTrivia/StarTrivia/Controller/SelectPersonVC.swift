82a3aa0deabf7a8d2ea793b1a053d3b4f882830f
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-10-28 10:10:13 -0700

    Animations added for fading buttons.

--- StarTrivia/StarTrivia/Controller/SelectPersonVC.swift
+++ StarTrivia/StarTrivia/Controller/SelectPersonVC.swift
@@ -27,22 +27,30 @@ class SelectPersonVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Step 1: make networking request
-
     }
     
     @IBAction func randomClicked(_ sender: Any) {
         let random = Int.random(in: 1 ... 87)
         personApi.getRandomPersonAlamo(id: random) { (person) in
             if let person = person {
-                self.nameLbl.text = person.name
-                self.heightLbl.text = person.height
-                self.massLbl.text = person.mass
-                self.hairLbl.text = person.hair
-                self.birthYearLbl.text = person.birthYear
-                self.genderLbl.text = person.gender
+                self.setupView(person: person)
             }
         }
+    }
+    
+    func setupView(person: Person) {
+        nameLbl.text = person.name
+        heightLbl.text = person.height
+        massLbl.text = person.mass
+        hairLbl.text = person.hair
+        birthYearLbl.text = person.birthYear
+        genderLbl.text = person.gender
+        
+        homeworldBtn.isEnabled = !person.homeworldUrl.isEmpty
+        vehiclesBtn.isEnabled = !person.vehicleUrls.isEmpty
+        starshipsBtn.isEnabled = !person.vehicleUrls.isEmpty
+        filmsBtn.isEnabled = !person.filmUrls.isEmpty
+        
     }
     
     @IBAction func homeworldClicked(_ sender: Any) {
@@ -53,6 +61,7 @@ class SelectPersonVC: UIViewController {
     
     @IBAction func starshipsClicked(_ sender: Any) {
     }
+    
     @IBAction func filmsClicked(_ sender: Any) {
     }
 }
