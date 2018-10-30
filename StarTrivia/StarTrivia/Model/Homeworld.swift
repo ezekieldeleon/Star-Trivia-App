6673e56cf681d9d92ec1236f04d8d6117b34dcc8
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-10-30 07:04:09 -0700

    We are now able to pass data to UI.

---
+++ StarTrivia/StarTrivia/Model/Homeworld.swift
@@ -0,0 +1,16 @@
+//
+//  Homeworld.swift
+//  StarTrivia
+//
+//  Created by Ezekiel De Leon on 10/30/18.
+//  Copyright © 2018 Ezekiel De Leon. All rights reserved.
+//
+
+import Foundation
+
+struct Homeworld: Codable {
+    let name: String
+    let climate: String
+    let terrain: String
+    let population: String
+}
