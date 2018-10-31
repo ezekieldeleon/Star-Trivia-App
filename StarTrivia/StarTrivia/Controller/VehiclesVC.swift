44306c6e12486007dbeba0165d8e11c488b33242
Ezekiel De Leon <ezekiel.d.e@ieee.org>
2018-10-31 07:04:40 -0700

    Finished adding api and model for out data.

--- StarTrivia/StarTrivia/Controller/VehiclesVC.swift
+++ StarTrivia/StarTrivia/Controller/VehiclesVC.swift
@@ -18,16 +18,39 @@ class VehiclesVC: UIViewController, PersonProtocol {
     @IBOutlet weak var lengthLbl: UILabel!
     @IBOutlet weak var speedLbl: UILabel!
     @IBOutlet weak var crewLbl: UILabel!
-    @IBOutlet weak var passangerLbl: UILabel!
+    @IBOutlet weak var passengerLbl: UILabel!
     @IBOutlet weak var previousBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
-    
     var person: Person!
+    let api = VehicleApi()
+    var vehicles = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(person.name)
+        vehicles = person.vehicleUrls
+        guard let firstVehicle = vehicles.first else { return }
+        getVehicle(url: firstVehicle)
+    }
+    
+    func getVehicle(url: String) {
+        api.getVehicle(url: url) { (vehicle) in
+            if let vehicle = vehicle {
+                self.setupView(vehicle: vehicle)
+            }
+        }
+    }
+    
+    func setupView(vehicle: Vehicle) {
+        nameLbl.text = vehicle.name
+        modelLbl.text = vehicle.model
+        makerLbl.text = vehicle.manufacturer
+        costLbl.text = vehicle.cost
+        lengthLbl.text = vehicle.length
+        speedLbl.text = vehicle.speed
+        crewLbl.text = vehicle.crew
+        passengerLbl.text = vehicle.passengers
+        
     }
     
     @IBAction func previousClicked(_ sender: Any) {
